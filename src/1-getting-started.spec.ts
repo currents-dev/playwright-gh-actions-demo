b0c2b0fdc1d1ca94289f85a1c8c60717fde09875
Richard Thornton <7372040+richthornton@users.noreply.github.com>
2023-02-01 16:44:46 -0800

    Add tests and Github Actions

---
+++ src/1-getting-started.spec.ts
@@ -0,0 +1,13 @@
+import {test, expect} from '@playwright/test';
+
+test('basic test', async ({page}) => {
+  await page.goto('https://todomvc.com/examples/vanilla-es6/');
+
+  // Use locators to represent a selector and re-use them
+  const inputBox = page.locator('input.new-todo');
+  const todoList = page.locator('.todo-list');
+
+  await inputBox.fill('Learn Playwright');
+  await inputBox.press('Enter');
+  await expect(todoList).toHaveText('Learn Playwright');
+});
