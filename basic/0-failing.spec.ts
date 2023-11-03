85223c0b53d9cb9bbe48f7a3a84d211f8093e4a3
Andrew Goldis <agoldis@gmail.com>
2023-11-03 15:15:27 -0700

    chore: add failing test

---
+++ basic/0-failing.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/test";
+
+test("basic test @basic", async ({ page }) => {
+  await page.goto("https://todomvc.com/examples/vanilla-es6/");
+
+  // Use locators to represent a selector and re-use them
+  const inputBox = page.locator("input.new-todo");
+  const todoList = page.locator(".todo-list");
+
+  await inputBox.fill("Stop using Cypress");
+  await inputBox.press("Enter");
+  await expect(todoList).toHaveText("Learn Playwright");
+});
