ef74fdd92af0bd89bbe8029401a32f301a3bbe18
Andrew Goldis <agoldis@gmail.com>
2024-03-20 15:51:54 -0700

    add pwc-p on 30 machines

--- basic/pw.config.shared.ts
+++ basic/pw.config.shared.ts
@@ -28,96 +28,12 @@ const config: PlaywrightTestConfig = {
 
   projects: [
     {
-      name: "chromium-00",
+      name: "chromium",
       retries: 2,
       use: {
         ...devices["Desktop Chrome"],
       },
     },
-    // {
-    //   name: "chromium-01",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-02",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-03",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-04",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-05",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-06",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-07",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-08",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-09",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-10",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-11",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: "chromium-12",
-    //   retries: 2,
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
