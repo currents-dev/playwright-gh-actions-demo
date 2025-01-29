601605542b4230c39728daf0332d31a41ed82439
Yohay Nahmany <yohay.arch@gmail.com>
2025-01-29 12:04:02 +0100

    chore: test

--- basic/0-failing.spec.ts
+++ basic/0-failing.spec.ts
@@ -4,6 +4,7 @@ import { test } from "./base.ts";
 test("basic test @basic", async ({ page }) => {
   await page.goto("https://demo.playwright.dev/todomvc");
 
+console.log("test");
   // Use locators to represent a selector and re-use them
   const inputBox = page.locator("input.new-todo");
   const todoList = page.locator(".todo-list");
