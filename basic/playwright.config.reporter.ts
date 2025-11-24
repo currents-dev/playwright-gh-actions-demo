17a9b2168f4e35fda5e8fe349ee70e53e4f2e0a3
Andrew Goldis <agoldis@gmail.com>
2025-11-24 11:20:25 -0800

    chore: demo

--- basic/playwright.config.reporter.ts
+++ basic/playwright.config.reporter.ts
@@ -1,10 +1,63 @@
 import { CurrentsConfig, currentsReporter } from "@currents/playwright";
-import config from "./pw.config.shared";
 
 const currentsConfig: CurrentsConfig = {
-  projectId: process.env.CURRENTS_PROJECT_ID ?? "xx",
-  recordKey: process.env.CURRENTS_RECORD_KEY ?? "yy",
-  outputFile: "currents-report.json",
+  projectId: "bnsqNa",
+  recordKey: "KPEvZL0LDYzcZH3U",
 };
 
-export default { ...config, reporter: [currentsReporter(currentsConfig)] };
+import { CurrentsFixtures, CurrentsWorkerFixtures } from "@currents/playwright";
+import { defineConfig, devices } from "@playwright/test";
+
+const config = defineConfig<CurrentsFixtures, CurrentsWorkerFixtures>({
+  timeout: 10 * 1000,
+
+  fullyParallel: true,
+
+  expect: {
+    timeout: 5000,
+  },
+
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [currentsReporter(currentsConfig)],
+
+  use: {
+    actionTimeout: 0,
+    trace: "on",
+    video: "on",
+    screenshot: "on",
+    // We can disable Currents fixtures if no project ID is provided
+    currentsFixturesEnabled: !!process.env.CURRENTS_PROJECT_ID,
+  },
+
+  projects: [
+    {
+      name: "Project A",
+      retries: 2,
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+    {
+      name: "Project B",
+      retries: 2,
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+  ],
+
+  webServer: {
+    command: "node ./server/index.js",
+    port: 4346,
+    reuseExistingServer: !process.env.CI,
+    stdout: "ignore",
+    stderr: "pipe",
+  },
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  outputDir: "test-results/",
+});
+
+export default config;
